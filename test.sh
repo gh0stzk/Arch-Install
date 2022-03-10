6650a78d715ebfcb2d363568252eb14e2ad0ef6f
gh0stzk <67278339+gh0stzk@users.noreply.github.com>
2022-03-10 08:40:38 -0600

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,33 +1,29 @@
 #!/bin/env bash
-pepito="yes"
-	if ping archlinux.org -W 3 -c 1 >/dev/null 2>&1; then
-			echo -e " Espera....\n"
-			sleep 3
-			echo -e "${CGR} Si hay Internet!!${CNC}"
-			sleep 2
-			clear
-		else
-			sleep 3
-			echo " Error: Parace que no hay internet.."
-				if [ $pepito = "yes" ]; then
-					read -p " Quieres intentar con una red WiFi? [s/n] " qwifi
+
+
+if [[ $(ping -W 3 -c 2 archlinux.org) != *" 0%"* ]]; then
+		echo " Error: Parace que no hay internet.."
+		
+			if [ "$(lspci -d ::280)" ]; then
+			
+				read -p "Intentar con WiFi? [s/N] " qwifi
 					if echo "$qwifi" | grep -iqF s; then
-						rfkill unblock all
 						device=$(ip link | grep "wl"* | grep -o -P "(?= ).*(?=:)" | sed -e "s/^[[:space:]]*//" | cut -d$'\n' -f 1)
-						printf "\nUsando WiFi...\n"
-						read -p " Nombre de tu red 'SSID': " ssid
-						read -rsp " WiFi Password: " wifipass
-						echo
-						iwctl --passphrase "$wifipass" station "$device" connect "$ssid"
-						
-					elif echo "$qwifi" | grep -iqF n; then
-						echo -e "${CGR} Saliendo..!!${CNC}"
-						exit 0
+					read -p "SSID: " ssid
+					read -rsp "WiFi Password: " wifipass
+					iwctl --passphrase "$wifipass" station "$device" connect "$ssid"
 					fi
-				fi
-				
-			echo " Saliendo...."
-			exit 0
-	fi
+			fi
+		else
+			echo "Si hay internet"
+fi
+
+if [[ $(ping -W 3 -c 2 archlinux.org) != *" 0%"* ]]; then
+        echo -en " No se ha podido establecer conexion, intenta conectarte via ethernet.
+        Si ingresaste mal los datos de tu wifi, vuelver a cargar el script e intenta de nuevo.
+        saliendo del script..."
+        sleep 10
+        exit 0
+fi
 		
 echo "sigue el script"
