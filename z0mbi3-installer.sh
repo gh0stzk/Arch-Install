97fb3325eea12840c437f0121173b3526823fc9a
gh0stzk <67278339+gh0stzk@users.noreply.github.com>
2022-03-26 17:29:16 -0600

    Add files via upload

--- z0mbi3-installer.sh
+++ z0mbi3-installer.sh
@@ -386,7 +386,7 @@ logo "Configurando SWAP"
 #----------------------------------------
 	
 logo "Particion NTFS de Windows para compartir almacenamiento"
-			lsblk -I 8 -o NAME,SIZE,PARTTYPENAME,FSTYPE,LABEL | grep "NTFS\|Microsoft"
+			lsblk -o NAME,SIZE,PARTTYPENAME,FSTYPE,LABEL | grep "NTFS\|Microsoft"
 			echo "------------------------------"
 			echo
 			PS3="Deseas montar una particion de almacenamiento compartida con WINDOWS, Escogela: "
@@ -622,7 +622,7 @@ logo "Aplicando optmizaciones.."
 	okie
     
 	echo -e "\n${CYE}Tunning ext4 file system for SSD and SpeedUp${CNC}"
-	sed -i 's/relatime/noatime,commit=120,barrier=0/' /mnt/etc/fstab
+	sed -i '0,/relatime/s/relatime/noatime,commit=120,barrier=0/' /mnt/etc/fstab
 	$CHROOT tune2fs -O fast_commit "${partroot}" >/dev/null
 	okie
     
@@ -958,7 +958,7 @@ echo -e "       /^.   \         os       $(source /mnt/etc/os-release && echo "$
 echo -e "      /  .-.  \        Kernel   $(uname -r)"   
 echo -e "     /  (   ) _\       pkgs     $(arch-chroot /mnt pacman -Q | wc -l)"
 echo -e "    / _.~   ~._^\      ram      $(free --mega | sed -n -E '2s/^[^0-9]*([0-9]+) *([0-9]+).*/''\2 MB/p')"
-echo -e "   /.^         ^.\     Disk     $(df -h / | grep "/" | awk '{print $3}')"
+echo -e "   /.^         ^.\     Disk     $(arch-chroot /mnt df -h / | grep "/" | awk '{print $3}')"
 		
 		echo
 		echo
